0fe16c0a1da84ecc36c40ce7ceab8cb83c5891f1
Filipe <joaomendonca2000@hotmail.com>
2025-07-28 22:03:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # showtrackr
 App to track series 
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ android/app/src/main/kotlin/com/example/showtrackr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.showtrackr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
