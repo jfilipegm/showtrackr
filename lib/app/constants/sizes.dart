3e3cbf184385591282302c5848ce04fd90a74045
Filipe <joaomendonca2000@hotmail.com>
2025-07-29 20:12:42 +0100

    feat: actual initial commit

---
+++ lib/app/constants/sizes.dart
@@ -0,0 +1,11 @@
+class Sizes {
+  static const double xxxs = 2;
+  static const double xxs = 4;
+  static const double xs = 8;
+  static const double s = 16;
+  static const double m = 24;
+  static const double l = 32;
+  static const double xl = 48;
+  static const double xxl = 64;
+  static const double xxxl = 96;
+}
